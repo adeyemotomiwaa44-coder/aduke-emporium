cadecbb6ee3c2b2d5f83bbee80111c1c83db3f4d
adeyemotomiwaa44-coder <adeyemotomiwaa44@gmail.com>
2026-04-26 17:36:28 +0100

    Add StoreLayout component with Navbar and Footer

---
+++ StoreLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import WhatsAppButton from './WhatsAppButton';
+import CartDrawer from './CartDrawer';
+
+export default function StoreLayout() {
+  return (
+    <div className="min-h-screen flex flex-col bg-background">
+      <Navbar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+      <WhatsAppButton />
+      <CartDrawer />
+    </div>
+  );
+}
